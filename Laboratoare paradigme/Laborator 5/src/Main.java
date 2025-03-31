0632e2604414985fc688065a84271408f1ca98c1
Nicu Stroia <nicu.stroia04@gmail.com>
2025-03-31 09:22:48 +0300

    scriere fisier

--- Laboratoare paradigme/Laborator 5/src/Main.java
+++ Laboratoare paradigme/Laborator 5/src/Main.java
@@ -1,17 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc;
-        List<Persoana> persoane = getPersoane();
+    public static void main(String[] args) throws IOException {
+        getPersoane();
+
+        String filePath="C:\\ProjectsGit\\PersonalProjects\\Laboratoare paradigme\\Laborator 5\\src\\DateSortate.txt";
+        File dateSortate=new File(filePath);
+        afisiareFisier(getPersoane(), dateSortate);
+    }
+
+    private static void afisiareFisier(List<Persoana> persoane, File dateSortate) throws IOException {
+        FileWriter writer = new FileWriter(dateSortate);
 
         persoane.sort(Comparator.comparing(Persoana -> Persoana.getNume()));
         for(Persoana persoana : persoane){
             System.out.println(persoana.toString());
+        }
+
+        for(Persoana persoana : persoane) {
+            writer.write(persoana.toString());
         }
     }
 
