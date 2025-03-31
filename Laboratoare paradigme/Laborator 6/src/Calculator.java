ba517c74e176c7cbdf4ecd71dce22c6973b3ad17
Nicu-Stroia <nicu.stroia04@gmail.com>
2025-03-31 10:22:24 +0300

    initial commit

---
+++ Laboratoare paradigme/Laborator 6/src/Calculator.java
@@ -0,0 +1,38 @@
+public class Calculator {
+    private double x;
+
+    public Calculator() {
+        this.x = 0;
+    }
+
+    public Calculator(double x) {
+        this.x = x;
+    }
+
+    public double Adunare(double numar){
+        x += numar;
+        return x;
+    }
+
+    public double Scadere(double numar){
+        x -= numar;
+        return x;
+    }
+
+    public double Inmultire(double numar){
+        x *= numar;
+        return x;
+    }
+
+    public double Impartire(double numar){
+        if(numar==0){
+            throw new ArithmeticException("Nu se poate imparti la 0");
+        }
+        x/=numar;
+        return x;
+    }
+
+    public void getRezultat(){
+        System.out.println("Rezultat " + x);
+    }
+}
---
+++ Laboratoare paradigme/Laborator 6/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator(5);
+        c.Adunare(2);
+        c.Scadere(1);
+        c.getRezultat();
+    }
+}
