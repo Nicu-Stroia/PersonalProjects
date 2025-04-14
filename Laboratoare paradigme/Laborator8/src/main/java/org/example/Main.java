3ee64a8fdfd2f27c1e05d2235fb7caf43eacf1ab
Nicu-Stroia <nicu.stroia04@gmail.com>
2025-04-14 14:33:56 +0300

    Sum of ages

--- Laboratoare paradigme/Laborator8/src/main/java/org/example/Main.java
+++ Laboratoare paradigme/Laborator8/src/main/java/org/example/Main.java
@@ -13,7 +13,9 @@ public class Main {
         try {
 
             // Reading file from local directory
-            FileInputStream file = new FileInputStream(new File("C:\\ProjectsGit\\PersonalProjects\\Laboratoare paradigme\\Laborator8\\src\\TestExcel.xlsx"));
+            FileInputStream file = new FileInputStream(
+                    new File("C:\\ProjectsGit\\PersonalProjects\\Laboratoare paradigme\\Laborator8\\src\\TestExcel.xlsx"));
+
             // Create Workbook instance holding reference to
             // .xlsx file
             XSSFWorkbook workbook = new XSSFWorkbook(file);
@@ -21,47 +23,69 @@ public class Main {
             // Get first/desired sheet from the workbook
             XSSFSheet sheet = workbook.getSheetAt(0);
 
-            XSSFWorkbook outputWorkbook = new XSSFWorkbook();
-            XSSFSheet outputSheet = outputWorkbook.createSheet("Modified");
+            // Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
 
-            int rowNum=0;
+            double sum = 0;
+            int count = 0;
 
-            for(Row row : sheet) {
-                Row newRow = outputSheet.createRow(rowNum++);
-
-                int cellnum = 0;
-
-                for(Cell cell : row) {
-                    Cell newCell = newRow.createCell(cellnum);
-
-                    if(cellnum==row.getLastCellNum()-1 && cell.getCellType() == CellType.NUMERIC){
-                        double newAge=cell.getNumericCellValue()+1;
-                        newCell.setCellValue(newAge);
-                    }
-                    else{
-                        switch (cell.getCellType()) {
-
-                            // Case 1
-                            case NUMERIC:
-                                newCell.setCellValue(cell.getNumericCellValue());
-                                break;
-
-                            // Case 2
-                            case STRING:
-                                newCell.setCellValue(cell.getStringCellValue());
-                                break;
-                        }
-                    }
-                    cellnum++;
+            for (Row row : sheet) {
+                Cell cell = row.getCell(2);
+                if (cell != null && cell.getCellType() == CellType.NUMERIC) {
+                    sum += cell.getNumericCellValue();
+                    count++;
                 }
             }
 
-            FileOutputStream outputFile = new FileOutputStream("C:\\ProjectsGit\\PersonalProjects\\Laboratoare paradigme\\Laborator8\\src\\Output.xlsx");
-            outputWorkbook.write(outputFile);
+            if (count > 0) {
+                Row row = sheet.getRow(0);
+                if (row == null) {
+                    row = sheet.createRow(0);
+                }
+                Cell cell = row.getCell(4);
+                if (cell == null) {
+                    cell = row.createCell(4);
+                }
+                cell.setCellValue(sum / count);
+            }
+            // Till there is an element condition holds true
+            while (rowIterator.hasNext()) {
 
-            outputFile.close();
-            workbook.close();
-            outputWorkbook.close();
+                Row row = rowIterator.next();
+
+                // For each row, iterate through all the
+                // columns
+                Iterator<Cell> cellIterator
+                        = row.cellIterator();
+
+                while (cellIterator.hasNext()) {
+
+                    Cell cell = cellIterator.next();
+
+                    // Checking the cell type and format
+                    // accordingly
+                    switch (cell.getCellType()) {
+
+                        // Case 1
+                        case NUMERIC:
+                            System.out.print(
+                                    cell.getNumericCellValue()
+                                            + " ");
+                            break;
+
+                        // Case 2
+                        case STRING:
+                            System.out.print(
+                                    cell.getStringCellValue()
+                                            + " ");
+                            break;
+                    }
+                }
+
+                System.out.println("");
+            }
+
+            // Closing file output streams
             file.close();
         }
 
