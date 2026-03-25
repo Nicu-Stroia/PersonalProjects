43c3a0a046f1597c6b43fb975aacac0ff80057a0
Nicu-Stroia <nicu.stroia04@gmail.com>
2026-03-25 09:41:28 +0200

    data types

---
+++ AI/Laborator1.py
@@ -0,0 +1,39 @@
+# user=input("Numele tau: ")
+# password = input("Parola ta: ")
+# criptedPassword = "*" * len(password)
+
+# print("Salut, " + user + " !")
+# print("Parola ta este " + criptedPassword)
+
+# l1=[1, 3.14, 'a', True]
+# l1 = l1 + [100]
+# l1 = l1 * 2
+# l1.pop(2)
+# l1.remove('a')
+l2=[1, 2, 4, 0]
+# l2.sort()
+# l3 = sorted([1, 2, 4, 0])
+# print(l3)
+
+# e1, e2, *rest = l2
+# print(e1, e2)
+
+m1=[[1, 2], 
+    [2, 3], 
+    [3, 4]]
+# print(m1)
+
+t1 = (1, 2, 3)
+# print(t1)
+
+# s1={1, 7, 7, 2}
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+# s3 = s1.union(s2)
+# s3 = s1.intersection(s2)
+# s3 = s1.difference(s2)
+# s3 = s1.symmetric_difference(s2)
+# print(s3)
+
+d1 = {'nume': 'Ana', 'varsta': 23, 'nota': 5}
+# print(d1)
